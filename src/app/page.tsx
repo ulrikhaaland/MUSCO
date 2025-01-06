2b23c7437bde7938ced6c722ae02a1ddc14f538d
Ulrik Håland <ulrikhaaland2@gmail.com>
2025-01-06 16:10:25 +0100

    page wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,12 @@ function HumanViewerWrapper() {
 
   return <HumanViewer gender={gender} onGenderChange={handleGenderChange} />;
 }
-
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <HumanViewerWrapper />
-    </Suspense>
+    <div className="overflow-y-auto">
+      <Suspense fallback={<div>Loading...</div>}>
+        <HumanViewerWrapper />
+      </Suspense>
+    </div>
   );
 }
