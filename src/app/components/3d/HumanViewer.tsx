4bf462603f8c3ea664f4d6196e1def8d2fe2110e
Ulrik Håland <ulrikhaaland2@gmail.com>
2025-01-06 09:35:19 +0100

    reset values on back to chat

--- src/app/components/3d/HumanViewer.tsx
+++ src/app/components/3d/HumanViewer.tsx
@@ -338,6 +338,7 @@ export default function HumanViewer({
   };
 
   const handleBack = () => {
+    resetValues();
     setShowQuestionnaire(false);
     setExerciseProgram(null);
     setIsGeneratingProgram(false);
